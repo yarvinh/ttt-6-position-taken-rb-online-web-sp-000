0531628923fb94ff94c35a4c3af968b35cd43aba
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-09 06:48:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board [index]
-    false
-  elsif index == x || o
-    true
+  #if board [index]
+    #false
+  #elsif index == x || o
+    #true
   end
 
 end
