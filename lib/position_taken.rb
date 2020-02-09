d45f24b7ad6049b12097908522a7de4efe3a47e1
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-09 07:26:29 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+   if board[index] == "X" || board[index] == "O"
+    true
+  elsif board[index]
+    false
+  elsif board[index] == nil
+    false
+  # elsif index == "O" || index == "X"
+  #   true
+# elsif board[index] == "X" || board[index] == "O"
+#     true
+  end
+
+end
+# position_taken?(["","","","",""], 2)
